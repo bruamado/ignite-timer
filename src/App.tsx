13a08cf5eef03a1e45684b434c58c25fc3606ef1
Bruno <bru-a@hotmail.com>
2024-10-25 21:22:40 -0300

    switching to notebook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
 
   return (
-    <>
-      <h1>React</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="danger"/>
+      <Button variant="success"/>
+    </ThemeProvider>
   )
 }
